9338bf50f0c22026710edc892bdd7b34a1fcfb15
catharinamega <67580484+catharinamega@users.noreply.github.com>
2021-04-16 14:55:48 +0700

    tes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
 </head>
 
 <body>
-    
+    <!-- DEN -->
     <!-- <div class="wrapper"> -->
                 <div class="side-left">
                 <div class="shortcut" onmouseover="showAdminProfile()">
