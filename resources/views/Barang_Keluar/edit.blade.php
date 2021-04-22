f3715ad48e7f0efc11708d81b0462be4a2b6baaf
denisaflorence <pflorence@student.ciputra.ac.id>
2021-04-23 00:02:57 +0800

    update barang masuk

--- resources/views/Barang_Keluar/edit.blade.php
+++ resources/views/Barang_Keluar/edit.blade.php
@@ -12,8 +12,56 @@
     position: relative;
     height: 100%;
 }
+.text-center {
+    margin-top: 20px;
+}
 </style>
-<form>
+<div class="card-body" style="margin-top: 40px">
+    <div class="row gutters">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+            <h5 class="mb-2 text-primary">Ubah Barang Keluar</h5>
+        </div>
+        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="form-group">
+                <label for="fullName">No. Nota</label>
+                {{-- <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="Oh Sehun"> --}}
+                <input type="text" class="form-control" id="fullName" value="J01001" readonly>
+            </div>
+            <div class="form-group">
+                <label for="fullName">Jumlah</label>
+                {{-- <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="Oh Sehun"> --}}
+                <input type="text" class="form-control" id="fullName" value="5">
+            </div>
+            <div class="form-group">
+                <label for="phone">Total Harga</label>
+                <input type="text" class="form-control" id="phone" value="1150000">
+            </div>
+        </div>
+        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="form-group">
+                <label for="eMail">Admin ID</label>
+                <input type="text" class="form-control" value="owner" readonly>
+            </div>
+            <div class="form-group">
+                <label for="eMail">Reseller ID</label>
+                <input type="text" class="form-control" value="RL001" readonly>
+            </div>
+            <div class="form-group">
+                <label for="eMail">Tanggal</label>
+                <input type="date" class="form-control" id="date" >
+            </div>
+        </div>
+    </div>
+    <div class="row gutters">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+            <div class="text-center">
+                <button type="button" id="submit" name="submit" class="btn btn-secondary btn-lg">Batal</button>
+                <button type="button" id="submit" name="submit" class="btn btn-primary btn-lg">Ubah</button>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- <form>
 	<div class="row">
 		<div class="col-sm">
 			<label for="notaa">No. Nota</label>
@@ -47,7 +95,7 @@
 	<div class="but " style="margin-left: 265px;margin-top: 20px;">
 		<button type="submit" class="btn  btn-primary mb-2 ">Ubah</button>
 	</div>
-</form>
+</form> --}}
 
 		<!--End Of Footer-->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
