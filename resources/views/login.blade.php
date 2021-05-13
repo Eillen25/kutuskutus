5078349af9fcb4755cc6ca027a0c8fae6bf580f6
denisaflorence <pflorence@student.ciputra.ac.id>
2021-05-13 20:21:48 +0800

    benerin alert

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -235,19 +235,19 @@ input {
 	<!-- ALERT -->
 	@if (Session::has('success'))
 	<div class="alert alert-success alert-block">
-	    <button type="button" class="close" data-dismiss="alert">×</button>
+	    {{-- <button type="button" class="close" data-dismiss="alert"></button> --}}
 	    <strong>{{ Session::get('success') }}</strong>
 	</div>
 	@endif
 	@if (Session::has('error'))
 	<div class="alert alert-danger alert-block">
-	    <button type="button" class="close" data-dismiss="alert">×</button>
+	    {{-- <button type="button" class="close" data-dismiss="alert"></button> --}}
 	    <strong>{{ Session::get('error') }}</strong>
 	</div>
 	@endif
 	@if (Session::has('keluar'))
 	<div class="alert alert-warning alert-block">
-	    <button type="button" class="close" data-dismiss="alert">×</button>
+	    {{-- <button type="button" class="close" data-dismiss="alert"></button> --}}
 	    <strong>{{ Session::get('keluar') }}</strong>
 	</div>
 	@endif
