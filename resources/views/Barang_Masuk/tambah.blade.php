5e71b5e16983cf4002f00034f49fde740a475814
Eillen25 <57930067+Eillen25@users.noreply.github.com>
2021-05-31 13:42:19 +0700

    readonly

--- resources/views/Barang_Masuk/tambah.blade.php
+++ resources/views/Barang_Masuk/tambah.blade.php
@@ -133,7 +133,7 @@
                 <input type="text" class="form-control mt-2" name="jumlah[]">
             </div>
             <div class="col-md-3">
-                <select class="form-control mt-2 ab-t-rpt-2" name="harga_satuan[]" >
+                <select class="form-control mt-2 ab-t-rpt-2" name="harga_satuan[]" readonly>
                     <option value="">--Harga Produk--</option>
                     @foreach($produk as $items)
                     <option value="{{ $items->harga_jual }}">{{$items->harga_jual}}</option>
